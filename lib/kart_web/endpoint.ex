846d82b2abadefd86dd51687aba0d1719b747aef
Jimmy Alford <jimmy.alford@salesloft.com>
2021-10-19 23:35:11 -0400

    additional config options for heroku deployment

--- lib/kart_web/endpoint.ex
+++ lib/kart_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule KartWeb.Endpoint do
   ]
 
   socket "/socket", KartWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
