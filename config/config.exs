90396cb3a6d4d3f8ec5604b06f9a2b37fe9a985c
Jimmy Alford <jimmy.alford@salesloft.com>
2021-10-23 00:24:22 -0400

    use only system envs

--- config/config.exs
+++ config/config.exs
@@ -28,10 +28,6 @@ config :phoenix, :json_library, Jason
 
 config :oauth2, debug: true
 
-config :oauth2_credentials,
-  client_id: System.get_env("CLIENT_ID"),
-  client_secret: System.get_env("CLIENT_SECRET"),
-
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
