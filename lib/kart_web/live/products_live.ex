e4518584cd1025c9a825088689714722d6de4d6d
Jimmy Alford <jimmy.alford@salesloft.com>
2021-10-24 01:07:38 -0400

    initial live view template for products search

---
+++ lib/kart_web/live/products_live.ex
@@ -0,0 +1,29 @@
+defmodule KartWeb.ProductsLive do
+  use KartWeb, :live_view
+
+  alias Kroger.ProductSearch
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, query: nil, products: nil)}
+  end
+
+  @impl true
+  def handle_event("search", %{"query" => query}, socket) do
+    products = ProductSearch.call(query, "01100461")
+    {:noreply, assign(socket, products: products)}
+  end
+
+  def render(assigns) do
+    ~L"""
+    <form phx-change="search">
+      <input type="text" name="q" value={@query} placeholder="Search..."/>
+      <datalist id="products">
+        <%= for product <- @products do %>
+          <option value={product}><%= product %></option>
+        <% end %>
+      </datalist>
+    </form>
+    """
+  end
+end
