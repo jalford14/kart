bffb68359db8d1bc12890f68f7349b5dedeeaa9e
Jimmy Alford <jimmy.alford@salesloft.com>
2021-10-24 01:08:54 -0400

    api logic for searching for products

--- lib/kroger/product_search.ex
+++ lib/kroger/product_search.ex
@@ -1,10 +1,12 @@
 defmodule Kroger.ProductSearch do
   alias Kroger.Utilities.ApiRequest
 
-  def call(term, location_id) do
+  def call(term, location_id, limit \\ 15) do
+    url(term, location_id, limit)
+    |> HTTPoison.get
   end
 
-  defp url(term, location_id) do
-    "https://api.kroger.com/v1/products?filter.term=#{term}&filter.locationId=#{location_id}"
+  defp url(term, location_id, limit) do
+    "https://api.kroger.com/v1/products?filter.term=#{term}&filter.locationId=#{location_id}?limit=#{limit}"
   end
 end
